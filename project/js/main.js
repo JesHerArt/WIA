ff07575601feda65171937eda9af60be64801b99
Jessica Hernandez <jesherart@fullsail.edu>
2015-06-21 17:28:40 -0400

    initial project files

---
+++ project/js/main.js
@@ -0,0 +1,13 @@
+/*
+    Jessica J. Hernandez
+    ID: 0004631401
+    June 25, 2015 - The Project
+    Website Interaction & Animation | 201506-01
+    Full Sail University
+*/
+
+$(document).ready(function() {
+
+	$(".fancybox").fancybox();
+ 
+});
