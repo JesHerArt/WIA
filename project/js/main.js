2f3b527aa7fc1bb1a0260312b4b110c1b81d4563
Jessica Hernandez <jesherart@fullsail.edu>
2015-06-22 23:24:39 -0400

    map and layout updates

--- project/js/main.js
+++ project/js/main.js
@@ -8,6 +8,30 @@
 
 $(document).ready(function() {
 
+    // MEET THE SWIRLS SECTION
+    // Fancybox use for the image gallery
 	$(".fancybox").fancybox();
- 
+    
+    
+    // LOCATION SECTION
+	// When a dot is clicked
+	$('a.dot').click(function(){
+
+		$('a.dot').removeClass('selected');
+		$(this).addClass('selected');
+
+		var city = '.location_detail#' + $(this).attr('city');
+		var htmlCode = $(city).html();
+        
+        console.log("city: " + city);
+        console.log("city html: " + htmlCode);
+
+		$('.detail_container').fadeOut(500, function(){
+			$('.detail_container .location_detail').html(htmlCode);
+			$('.detail_container').fadeIn(500);
+		});
+
+	});
+    
+    
 });
